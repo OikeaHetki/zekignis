b2bd49eb377d6eca2dd3f727b031c770b610d51d
OikeaHetki <72877284+OikeaHetki@users.noreply.github.com>
2022-05-28 06:55:00 -0400

    Add files via upload

--- script/c2610021435.lua
+++ script/c2610021435.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,97567736,aux.FilterBoolFunctionEx(Card.IsSetCard,0xa9))
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,0xc3),aux.FilterBoolFunctionEx(Card.IsSetCard,0xa9))
 	--pos change
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_POSITION)
