320d6a824b6a579b1376987cf760d91faf3719fb
OikeaHetki <zakktrez@gmail.com>
2022-05-28 10:08:54 -0400

    Fix for missing card arts

---
+++ script/c2606101997.lua
@@ -0,0 +1,58 @@
+--Frightfur Kief-Kong
+--zekpro card
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,30068120,aux.FilterBoolFunctionEx(Card.IsSetCard,0xa9))
+	--actlimit upon attacking
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(s.aclimit)
+	e1:SetCondition(s.actcon)
+	c:RegisterEffect(e1)
+	--atk limit (opponent)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetValue(s.atlimit)
+	c:RegisterEffect(e2)
+	--battle dam 0
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--Must attack
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_MUST_ATTACK)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(0,LOCATION_MZONE)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EFFECT_MUST_ATTACK_MONSTER)
+	e5:SetValue(s.atklimit)
+	c:RegisterEffect(e5)
+end
+s.listed_series={0xa9}
+s.material_setcode={0xa9,0xc3}
+function s.atklimit(e,c)
+	return c==e:GetHandler()
+end
+function s.aclimit(e,re,tp)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function s.actcon(e)
+	return Duel.GetAttacker()==e:GetHandler() or Duel.GetAttackTarget()==e:GetHandler()
+end
+function s.atlimit(e,c)
+	return c~=e:GetHandler()
+end
