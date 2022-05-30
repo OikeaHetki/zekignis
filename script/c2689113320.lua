9055a3beda1483995435853a708135cdadca80ec
OikeaHetki <zakktrez@gmail.com>
2022-05-29 20:48:02 -0400

    FF

--- script/c2689113320.lua
+++ script/c2689113320.lua
@@ -3,7 +3,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--pendulum summon
-	Pendulum.AddProcedure(c)
+	Pendulum.AddProcedure(c,false)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(1160)
