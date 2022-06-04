9ff02fa2bbee63b580ebfc9c42d3553d97e29cf2
OikeaHetki <zakktrez@gmail.com>
2022-06-04 17:28:39 -0400

    heheh

--- script/c2600003118.lua
+++ script/c2600003118.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
-	Link.AddProcedure(c,s.matfilter,3,3)
+	Link.AddProcedure(c,s.matfilter,3,3,s.check)
 	--cannot link material
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -29,6 +29,9 @@ function s.initial_effect(c)
 	e4:SetValue(1)
 	c:RegisterEffect(e4)
 end
-function s.matfilter(c)
-	return c:IsRace(RACE_DRAGON) and c:IsType(TYPE_EFFECT) and c:IsLevelAbove(7)
+function s.matfilter(g,lc,sumtype,tp)
+	return c:IsRace(RACE_DRAGON,g,lc,sumtype,tp) and c:IsType(TYPE_EFFECT,g,lc,sumtype,tp)
+end
+function s.check(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsLevelAbove,7)
 end
