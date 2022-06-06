e2676b33790e9eac50af05886e04a67dba522c45
OikeaHetki <zakktrez@gmail.com>
2022-06-06 12:26:11 -0400

    Sagan's Dragon

--- script/c2600003119.lua
+++ script/c2600003119.lua
@@ -1,19 +1,14 @@
---フレイム・バッファロー
+--Sagan's Cosmos Dragon
+--zek
 local s,id=GetID()
 function s.initial_effect(c)
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD)
-	e0:SetCode(EFFECT_PUBLIC)
-	e0:SetRange(LOCATION_MZONE)
-	e0:SetTargetRange(0,LOCATION_HAND)
-	c:RegisterEffect(e0)
 	--Special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e1:SetRange(LOCATION_HAND)
+	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
 	e1:SetCode(EVENT_SUMMON_NEGATED)
 	e1:SetCondition(s.sumcon)
 	e1:SetTarget(s.sumtg)
@@ -35,20 +30,46 @@ function s.initial_effect(c)
 	local e4=e1:Clone()
 	e4:SetCode(EVENT_FLIP_SUMMON_NEGATED)
 	c:RegisterEffect(e4)
-	--draw
+	local e5=e1:Clone()
+	e5:SetCode(EVENT_CHAINING)
+	e5:SetCondition(s.chcon)
+	c:RegisterEffect(e5)
+	--todeck
 	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(id,0))
-	e6:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
-	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e6:SetCode(EVENT_LEAVE_FIELD)
-	e6:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DELAY)
-	e6:SetCountLimit(1,id)
-	e6:SetCondition(s.condition)
-	e6:SetTarget(s.target)
-	e6:SetOperation(s.operation)
+	e6:SetDescription(aux.Stringid(id,1))
+	e6:SetCategory(CATEGORY_TODECK+CATEGORY_RECOVER)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCode(EVENT_PHASE+PHASE_END)
+	e6:SetCountLimit(1)
+	e6:SetTarget(s.rettg)
+	e6:SetOperation(s.retop)
 	c:RegisterEffect(e6)
+	--draw
+	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(id,0))
+	e7:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
+	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e7:SetCode(EVENT_LEAVE_FIELD)
+	e7:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DELAY)
+	e7:SetCountLimit(1,id)
+	e7:SetCondition(s.condition)
+	e7:SetTarget(s.target)
+	e7:SetOperation(s.operation)
+	c:RegisterEffect(e7)
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_FIELD)
+	e8:SetCode(EFFECT_PUBLIC)
+	e8:SetRange(LOCATION_MZONE)
+	e8:SetTargetRange(0,LOCATION_HAND)
+	c:RegisterEffect(e8)
 end
 ---negsum
+function s.chcon(e,tp,eg,ep,ev,re,r,rp)
+	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
+	return (loc==LOCATION_HAND or loc==LOCATION_GRAVE) and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
+			and Duel.GetCurrentPhase()==PHASE_MAIN1 and Duel.IsTurnPlayer(tp)
+end
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 	local de,dp=Duel.GetChainInfo(ev,CHAININFO_DISABLE_REASON,CHAININFO_DISABLE_PLAYER)
 	if de then
@@ -88,5 +109,24 @@ end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.DiscardHand(tp,s.tgfilter,1,1,REASON_EFFECT+REASON_DISCARD)~=0 then
 		Duel.Draw(tp,2,REASON_EFFECT)
+	end
+end
+--ret
+function s.filter(c)
+	return c:IsRace(RACE_DRAGON) and c:IsType(TYPE_MONSTER) and c:IsAbleToDeck() and not c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_REMOVED,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_REMOVED,0,1,99,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,#g*300)
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	local ct=Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	if ct>0 then
+		Duel.Recover(tp,ct*300,REASON_EFFECT)
 	end
 end
