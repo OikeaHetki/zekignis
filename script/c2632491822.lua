44060b8fc68ef9d90a42df91bf9e2f45a1f117f9
OikeaHetki <zakktrez@gmail.com>
2022-06-14 09:48:14 -0400

    Hamon

--- script/c2632491822.lua
+++ script/c2632491822.lua
@@ -50,10 +50,10 @@ function s.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 function s.spfilter(c)
-	return (c:IsFaceup() and c:GetType()==TYPE_SPELL and c:IsAbleToGraveAsCost()) or (c:IsFacedown() and c:IsType(TYPE_SPELL) and c:IsAbleToGraveAsCost())
+	return c:IsType(TYPE_SPELL) and c:IsAbleToGraveAsCost()
 end
 function s.exfilter(c)
-	return s.spfilter(c) or (c:IsFacedown() and c:IsType(TYPE_SPELL) and c:IsAbleToGraveAsCost())
+	return s.spfilter(c) or c:IsType(TYPE_SPELL) and c:IsAbleToGraveAsCost()
 end
 function s.spcon(e,c)
 	if c==nil then return true end
