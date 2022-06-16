39a6b56ce6a5045751f339fddff86c86e96ef672
OikeaHetki <zakktrez@gmail.com>
2022-06-16 18:27:10 -0400

    slifer again

--- script/c2605253985.lua
+++ script/c2605253985.lua
@@ -25,20 +25,6 @@ function s.initial_effect(c)
 	e3:SetCondition(s.drcon1)
 	e3:SetOperation(s.drop1)
 	c.RegisterEffect(e3)
-	--sp_summon effect
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e4:SetCondition(s.regcon)
-	e4:SetOperation(s.regop)
-	c.RegisterEffect(e4)
-	--sp2draw
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e5:SetCode(EVENT_CHAIN_SOLVED)
-	e5:SetCondition(s.drcon2)
-	e5:SetOperation(s.drop2)
-	c.RegisterEffect(e5)
 end
 s.listed_names={10000020}
 ---condition
@@ -54,23 +40,7 @@ function s.filter(c,sp)
 end
 function s.drcon1(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.filter,1,nil,1-tp) 
-		and (not re:IsHasType(EFFECT_TYPE_ACTIONS) or re:IsHasType(EFFECT_TYPE_CONTINUOUS))
 end
 function s.drop1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Draw(tp,1,REASON_EFFECT)
-end
-function s.regcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.filter,1,nil,1-tp) 
-		and re:IsHasType(EFFECT_TYPE_ACTIONS) and not re:IsHasType(EFFECT_TYPE_CONTINUOUS)
-end
-function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,1)
-end
-function s.drcon2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,id)>0
-end
-function s.drop2(e,tp,eg,ep,ev,re,r,rp)
-	local n=Duel.GetFlagEffect(tp,id)
-	Duel.ResetFlagEffect(tp,id)
-	Duel.Draw(tp,n,REASON_EFFECT)
 end
