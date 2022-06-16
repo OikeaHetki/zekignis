98d7ee58b52f0cf0d2fdc1b0e489b16edd2bec58
OikeaHetki <zakktrez@gmail.com>
2022-06-16 18:42:14 -0400

    Test

--- script/c2605253985.lua
+++ script/c2605253985.lua
@@ -17,6 +17,27 @@ function s.initial_effect(c)
 	e2:SetTargetRange(1,0)
 	e2:SetValue(100)
 	c:RegisterEffect(e2)
+	--draw when spsum from gy
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCondition(s.con)
+	e3:SetTarget(s.tg)
+	e3:SetOperation(s.op)
+	c:RegisterEffect(e3)
+	--Activate
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetTarget(s.targ)
+	e4:SetOperation(s.des)
+	c:RegisterEffect(e4)
 end
 s.listed_names={10000020}
 ---condition
@@ -25,4 +46,44 @@ function s.cfilter(c)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+---draw
+function s.gfilter(c,tp)
+	return c:IsPreviousLocation(LOCATION_GRAVE) and c:IsPreviousControler(tp) 
+	and (c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_AQUA))
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	if #g~=2 then return false end
+	local c=g:GetFirst()
+	return c:IsFaceup() and c:IsCode(10000020) and c:IsOriginalAttribute(ATTRIBUTE_DIVINE)
+	and eg:IsExists(s.gfilter,1,nil,tp)
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,3) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(3)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,3)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
+---des
+function s.filter(c,e,tp)
+	return c:IsSummonPlayer(tp) and c:IsDefensePos() and (not e or (c:IsRelateToEffect(e) and c:IsLocation(LOCATION_MZONE)))
+end
+function s.targ(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg and eg:IsExists(s.filter,1,nil,nil,1-tp) end
+	local g=eg:Filter(s.filter,nil,nil,1-tp)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.des(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local g=tg:Filter(s.filter,nil,e,1-tp)
+	if #g>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
 end
