4ce7f5b2bca3471f5f2f77b21a280e76ddf4b7f6
OikeaHetki <zakktrez@gmail.com>
2022-06-16 18:31:19 -0400

    base test

--- script/c2605253985.lua
+++ script/c2605253985.lua
@@ -17,14 +17,6 @@ function s.initial_effect(c)
 	e2:SetTargetRange(1,0)
 	e2:SetValue(100)
 	c:RegisterEffect(e2)
-	--draw
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCondition(s.drcon1)
-	e3:SetOperation(s.drop1)
-	c.RegisterEffect(e3)
 end
 s.listed_names={10000020}
 ---condition
@@ -33,19 +25,4 @@ function s.cfilter(c)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
-end
----each time, draw
-function s.cfilter1(c)
-	return c:IsFaceup() and c:IsCode(10000020)
-end
-function s.filter(c,sp)
-	return c:GetSummonPlayer()==sp
-end
-function s.drcon1(e,tp,eg,ep,ev,re,r,rp)
-	local tp=e:GetHandlerPlayer()
-	return eg:IsExists(s.filter,1,nil,1-tp) 
-	and Duel.IsExistingMatchingCard(s.cfilter1,tp,LOCATION_ONFIELD,0,1,nil)
-end
-function s.drop1(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Draw(tp,1,REASON_EFFECT)
 end
