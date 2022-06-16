67ed162cdf96ce37e15862712b02b789cfde03d8
OikeaHetki <zakktrez@gmail.com>
2022-06-16 18:18:15 -0400

    Power of Slifer

--- script/c2605253985.lua
+++ script/c2605253985.lua
@@ -1,54 +1,79 @@
---交差する魂
---Soul Crossing
---zekpro version
---by Rundas, with tweaks from zek
+--Power of the Sky Dragon
+--ZekPro
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetCondition(s.condition)
 	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_HAND_LIMIT)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(1,0)
+	e2:SetValue(100)
+	c:RegisterEffect(e2)
+	--draw
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCondition(s.drcon1)
+	e3:SetOperation(s.drop1)
+	e3:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e3)
+	--sp_summon effect
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetCondition(s.regcon)
+	e4:SetOperation(s.regop)
+	e4:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e4)
+	--sp2draw
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e5:SetCode(EVENT_CHAIN_SOLVED)
+	e5:SetCondition(s.drcon2)
+	e5:SetOperation(s.drop2)
+	e5:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e5)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g1,g2,g3,g4=Duel.GetMatchingGroup(Card.IsReleasable,tp,LOCATION_MZONE,0,nil),Duel.GetMatchingGroup(Card.IsReleasable,tp,0,LOCATION_MZONE,nil),Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_HAND+LOCATION_ONFIELD,0,e:GetHandler()),
-	Duel.GetMatchingGroup(Card.IsRace,tp,LOCATION_HAND,0,nil,RACE_DIVINE)
-	if chk==0 then
-		if #g4==1 then
-			return #g4>0 and (aux.SelectUnselectGroup(g1+g2,e,tp,3,3,s.rescon(g1,g2,g3-g4,g4),0) or #g1>=3)
-		else
-			return #g4>0 and (aux.SelectUnselectGroup(g1+g2,e,tp,3,3,s.rescon(g1,g2,g3,g4),0) or #g1>=3)
-		end
-	end
+s.listed_names={10000020}
+---condition
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsCode(10000020) and c:IsOriginalAttribute(ATTRIBUTE_DIVINE)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g1,g2,g3,g4=Duel.GetMatchingGroup(Card.IsReleasable,tp,LOCATION_MZONE,0,nil),Duel.GetMatchingGroup(Card.IsReleasable,tp,0,LOCATION_MZONE,nil),Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_HAND+LOCATION_ONFIELD,0,e:GetHandler()),
-	Duel.GetMatchingGroup(Card.IsRace,tp,LOCATION_HAND,0,nil,RACE_DIVINE)
-	if not (Duel.IsExistingMatchingCard(Card.IsRace,tp,LOCATION_HAND,0,1,nil,RACE_DIVINE) and (aux.SelectUnselectGroup(g1+g2,e,tp,3,3,s.rescon(g1,g2,g3,g4),0) or #g1>=3)) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local tc=Duel.SelectMatchingCard(tp,Card.IsRace,tp,LOCATION_HAND,0,1,1,nil,RACE_DIVINE):GetFirst()
-	Duel.ConfirmCards(1-tp,tc)
-	local choice=aux.SelectEffect(tp,{aux.SelectUnselectGroup(g1+g2,e,tp,3,3,s.rescon(g1,g2,g3,g4),0),aux.Stringid(id,1)},{#g1>=3,aux.Stringid(id,2)})
-	if choice==2 then
-		Duel.Summon(tp,tc,false,nil)
-	else
-		local g=aux.SelectUnselectGroup(g1+g2,e,tp,3,3,s.rescon(g1,g2,g3,g4),1,tp,HINTMSG_RELEASE,nil,nil)
-		Duel.HintSelection(g)
-		local g5=aux.SelectUnselectGroup(g3-g-Group.FromCards(tc),e,tp,#(g&g2),#(g&g2),aux.ChkfMMZ(1),1,tp,HINTMSG_TOGRAVE,nil,nil)
-		Duel.Release(g,REASON_EFFECT)
-		Duel.SendtoGrave(g5,REASON_EFFECT)
-		Duel.MoveToField(tc,tp,tp,LOCATION_MZONE,POS_FACEUP_ATTACK,true)
-	end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
-function s.rescon(g1,g2,g3,g4)
-	return function(sg,e,tp,mg)
-		if #g4>1 then
-			return #(sg&g2)<=#g3
-		else
-			return #(sg&g2)<=#g3-1
-		end
-	end
+---each time, draw
+function s.filter(c,sp)
+	return c:GetSummonPlayer()==sp
+end
+function s.drcon1(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter,1,nil,1-tp) 
+		and (not re:IsHasType(EFFECT_TYPE_ACTIONS) or re:IsHasType(EFFECT_TYPE_CONTINUOUS))
+end
+function s.drop1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Draw(tp,1,REASON_EFFECT)
+end
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter,1,nil,1-tp) 
+		and re:IsHasType(EFFECT_TYPE_ACTIONS) and not re:IsHasType(EFFECT_TYPE_CONTINUOUS)
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,1)
+end
+function s.drcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFlagEffect(tp,id)>0
+end
+function s.drop2(e,tp,eg,ep,ev,re,r,rp)
+	local n=Duel.GetFlagEffect(tp,id)
+	Duel.ResetFlagEffect(tp,id)
+	Duel.Draw(tp,n,REASON_EFFECT)
 end
