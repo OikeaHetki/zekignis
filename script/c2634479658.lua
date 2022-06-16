3cd95b05cb894801e3d16fa29ba93a5c881eee23
OikeaHetki <zakktrez@gmail.com>
2022-06-16 19:21:28 -0400

    jams

---
+++ script/c2634479658.lua
@@ -0,0 +1,80 @@
+--King of Jams & Slimes
+--zek
+local s,id=GetID()
+function s.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+   --reborn preparation
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_DESTROYED)
+    e2:SetCondition(s.spconx)
+    e2:SetOperation(s.spopx)
+    c:RegisterEffect(e2)
+    --reborn
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+    e3:SetRange(LOCATION_GRAVE)
+    e3:SetCountLimit(1)
+    e3:SetCondition(s.spcon2)
+    e3:SetOperation(s.spop2)
+    c:RegisterEffect(e3)
+end
+function s.spconx(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.spopx(e,tp,eg,ep,ev,re,r,rp)
+    e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,0)
+end
+function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():GetFlagEffect(id)>0
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    c:ResetFlagEffect(id)
+    Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+end
+function s.filter(c,e,tp)
+	return c:IsRace(RACE_AQUA) and c:IsAttribute(ATTRIBUTE_WATER) and not c:IsCode(id) 
+	and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsSummonableCard()
+end
+function s.hsfilter(c)
+	return not c:IsPublic() or c:IsType(TYPE_MONSTER)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil,TYPE_MONSTER) 
+		and	not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,e:GetHandler(),e,tp) end
+	local dg=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_HAND+LOCATION_MZONE,0,nil,TYPE_MONSTER)
+	if not Duel.IsExistingMatchingCard(s.hsfilter,tp,LOCATION_HAND,0,1,nil) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,1,tp,LOCATION_HAND+LOCATION_MZONE)
+	else
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_HAND+LOCATION_MZONE)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,e:GetHandler(),e,tp)
+	if #g>0 then
+		g:AddCard(e:GetHandler())
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
