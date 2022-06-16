2b0aa6575f06a26017ab2876de501040a7c51507
OikeaHetki <zakktrez@gmail.com>
2022-06-16 19:27:59 -0400

    test

--- script/c2634479658.lua
+++ script/c2634479658.lua
@@ -13,38 +13,38 @@ function s.initial_effect(c)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
    --reborn preparation
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e2:SetProperty(EFFECT_FLAG_DELAY)
-    e2:SetCode(EVENT_DESTROYED)
-    e2:SetCondition(s.spconx)
-    e2:SetOperation(s.spopx)
-    c:RegisterEffect(e2)
-    --reborn
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-    e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
-    e3:SetRange(LOCATION_GRAVE)
-    e3:SetCountLimit(1)
-    e3:SetCondition(s.spcon2)
-    e3:SetOperation(s.spop2)
-    c:RegisterEffect(e3)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetCondition(s.spconx)
+	e2:SetOperation(s.spopx)
+	c:RegisterEffect(e2)
+	--reborn
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.spcon2)
+	e3:SetOperation(s.spop2)
+	c:RegisterEffect(e3)
 end
 function s.spconx(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
 function s.spopx(e,tp,eg,ep,ev,re,r,rp)
-    e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,0)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,0)
 end
 function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():GetFlagEffect(id)>0
+	return e:GetHandler():GetFlagEffect(id)>0
 end
 function s.spop2(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    c:ResetFlagEffect(id)
-    Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	local c=e:GetHandler()
+	c:ResetFlagEffect(id)
+	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.filter(c,e,tp)
 	return c:IsRace(RACE_AQUA) and c:IsAttribute(ATTRIBUTE_WATER) and not c:IsCode(id) 
@@ -55,7 +55,7 @@ function s.hsfilter(c)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil,TYPE_MONSTER) 
-		and	not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,e:GetHandler(),e,tp) end
@@ -71,10 +71,13 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g1=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil,TYPE_MONSTER)
+	if #g1>0 and Duel.Destroy(g1,REASON_EFFECT)~=0 then
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,e:GetHandler(),e,tp)
-	if #g>0 then
-		g:AddCard(e:GetHandler())
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	local g2=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,e:GetHandler(),e,tp)
+	if #g2>0 then
+		g2:AddCard(e:GetHandler())
+		Duel.SpecialSummon(g2,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
