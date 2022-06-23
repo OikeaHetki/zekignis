5413f0d89e7cbe82eb2eb4adc5ec347bc98f9fbf
OikeaHetki <zakktrez@gmail.com>
2022-06-23 11:00:50 -0400

    scake

--- script/c2600001253.lua
+++ script/c2600001253.lua
@@ -25,13 +25,13 @@ function s.initial_effect(c)
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
 	--change name
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetCode(EFFECT_CHANGE_CODE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetValue(46986414)
-	c:RegisterEffect(e3)
+  --  local e3=Effect.CreateEffect(c)
+  --  e3:SetType(EFFECT_TYPE_SINGLE)
+  --  e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+  --  e3:SetCode(EFFECT_CHANGE_CODE)
+  --  e3:SetRange(LOCATION_MZONE)
+  --  e3:SetValue(46986414)
+  --  c:RegisterEffect(e3)
 end
 s.listed_names={CARD_DARK_MAGICIAN}
 --sphand
@@ -41,7 +41,7 @@ end
 function s.spcon(e,c)
 	if c==nil then return true end
 	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
-		and	Duel.IsExistingMatchingCard(s.filter2,c:GetControler(),LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.filter2,c:GetControler(),LOCATION_MZONE,0,1,nil)
 end
 ---tohand
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
