ea2e56a5d429d755632fe5f8bf3e7ea708934dd8
OikeaHetki <zakktrez@gmail.com>
2022-06-23 21:37:03 -0400

    test

--- script/c2600006556.lua
+++ script/c2600006556.lua
@@ -8,17 +8,16 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x47}
 function s.filter(c,e,tp)
-    return c:IsSetCard(0x47) and not c:IsSetCard(0x1047) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x47) and not c:IsSetCard(0x1047) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.thfilter(c,e,tp)
-    return c:IsSetCard(0x47) and c:IsAbleToHand()
+	return c:IsSetCard(0x47) and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
