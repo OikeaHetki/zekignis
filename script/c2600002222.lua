95aac91f2d9b0d679bbaacf4913a5b7763c39235
OikeaHetki <zakktrez@gmail.com>
2022-06-25 21:01:37 -0400

    test

--- script/c2600002222.lua
+++ script/c2600002222.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
 	--Reflect effect change
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
