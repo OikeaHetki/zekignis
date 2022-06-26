185810ab8768a52558b5538c43a38393d25fb789
OikeaHetki <zakktrez@gmail.com>
2022-06-25 20:52:03 -0400

    testtest

--- script/c2600002222.lua
+++ script/c2600002222.lua
@@ -22,8 +22,6 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
 		--Any effect that would inflict damage increases LP instead
 		local e1=Effect.CreateEffect(c)
 		e1:SetDescription(aux.Stringid(id,2))
@@ -34,7 +32,6 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(s.revval)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
-	end
 end
 function s.revval(e,re,r,rp,rc)
 	return (r&REASON_EFFECT)~=0
