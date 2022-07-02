f6be560c4e31b9cf0e72b00f02188a57604ef82c
OikeaHetki <zakktrez@gmail.com>
2022-07-01 21:23:39 -0400

    test

--- script/c2675732622.lua
+++ script/c2675732622.lua
@@ -21,15 +21,15 @@ function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>=2 and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
-		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
 		and Duel.IsPlayerCanSpecialSummonCount(tp,2)
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,75732623+1,0,TYPES_TOKEN,0,0,1,RACE_FIEND,ATTRIBUTE_DARK)
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,75732623,0,TYPES_TOKEN,0,0,1,RACE_FIEND,ATTRIBUTE_DARK)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	for i=1,2 do
-		local token=Duel.CreateToken(tp,75732623+1)
+		local token=Duel.CreateToken(tp,75732623)
 		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_ATTACK)
 	end
+	Duel.SpecialSummonComplete()
 	Duel.SpecialSummonComplete()
 	local ge1=Effect.CreateEffect(e:GetHandler())
 	ge1:SetType(EFFECT_TYPE_FIELD)
