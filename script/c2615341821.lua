eada4714496f30ce35d3c35459f3a88170e0badc
OikeaHetki <zakktrez@gmail.com>
2022-07-01 21:27:32 -0400

    dan

--- script/c2615341821.lua
+++ script/c2615341821.lua
@@ -22,7 +22,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<2 
-		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,1,RACE_PLANT,ATTRIBUTE_WIND) then return end
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,15341822,0,TYPES_TOKEN,0,0,1,RACE_PLANT,ATTRIBUTE_WIND) then return end
 	for i=1,2 do
 		local token=Duel.CreateToken(tp,id+i)
 		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
