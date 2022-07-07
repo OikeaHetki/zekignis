623127d5572f3a5b2c7b4312d95d635fac1ea882
OikeaHetki <zakktrez@gmail.com>
2022-07-06 20:27:06 -0400

    test

--- script/c2621105106.lua
+++ script/c2621105106.lua
@@ -76,7 +76,6 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTargetRange(1,0)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetTarget(s.splimit)
-	e1:SetLabel(fid)
 	Duel.RegisterEffect(e1,tp)
 end
 function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
