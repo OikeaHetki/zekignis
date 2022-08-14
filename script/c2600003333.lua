cd63a4b8c9df15c2ef46c83cbabf751ab43fa869
OikeaHetki <zakktrez@gmail.com>
2022-08-14 07:47:38 -0400

    test

--- script/c2600003333.lua
+++ script/c2600003333.lua
@@ -37,7 +37,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			Duel.ConfirmCards(1-tp,sg)
 		end
 	end
-	local e1=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
