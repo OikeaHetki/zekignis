aa64b098a32084f9599fd8cc993652cde18323a7
OikeaHetki <zakktrez@gmail.com>
2022-08-16 11:45:12 -0400

    test

--- script/c2606337436.lua
+++ script/c2606337436.lua
@@ -64,7 +64,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Damage(p,d,REASON_EFFECT)
 end
 --dragon's rage condition
-function s.drccon(e)
+function s.drccon(e,tp,eg)
 	local g1=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_MONSTER)
 	local g2=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)
 	local g3=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_TRAP)
