aa74dc5ae3249495d3c803bc75de6549bf3b6a7c
OikeaHetki <zakktrez@gmail.com>
2022-08-16 11:42:37 -0400

    drc

--- script/c2606337436.lua
+++ script/c2606337436.lua
@@ -65,9 +65,10 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 end
 --dragon's rage condition
 function s.drccon(e)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_SPELL),tp,LOCATION_GRAVE,0,1,nil)
-		and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_TRAP),tp,LOCATION_GRAVE,0,1,nil)
-			and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_MONSTER),tp,LOCATION_GRAVE,0,1,nil)
+	local g1=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_MONSTER)
+	local g1=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)
+	local g1=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_TRAP)
+	return #g1>0 and #g1>0 and #g3>0
 end
 --surveil operation
 function s.survcost(e,tp,eg,ep,ev,re,r,rp,chk)
