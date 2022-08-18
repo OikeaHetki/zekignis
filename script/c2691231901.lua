39fe6bfcb843697187fbb56206e927348e2a2e35
OikeaHetki <zakktrez@gmail.com>
2022-08-17 22:02:50 -0400

    hazy finale

--- script/c2691231901.lua
+++ script/c2691231901.lua
@@ -23,16 +23,24 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 s.listed_series={0x7d}
-function s.filter(c,tp)
-	return c:IsSetCard(0x7d) and c:GetActivateEffect():IsActivatable(tp,true,true) and c:IsType(TYPE_SPELL+TYPE_TRAP) and not c:IsCode(id)
+function s.setfilter(c)
+	return c:IsSetCard(0x7d) and c:IsSSetable() and c:IsType(TYPE_SPELL+TYPE_TRAP)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
-	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
-	Duel.MoveToField(tc,tp,1-tp,LOCATION_SZONE,POS_FACEUP,true)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.setfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 and Duel.SSet(tp,g)>0 and g:GetFirst():IsPreviousLocation(LOCATION_DECK) then
+		--Can be activated the turn it was Set
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+		e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		g:GetFirst():RegisterEffect(e1)
+	end
 end
 function s.thfilter(c)
 	return c:IsSetCard(0x107d) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
