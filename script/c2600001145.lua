60e1b69443909c99d00ac3da56c27b160e4bd411
OikeaHetki <zakktrez@gmail.com>
2022-08-30 12:40:47 -0400

    test

--- script/c2600001145.lua
+++ script/c2600001145.lua
@@ -85,18 +85,7 @@ function s.dredgeop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.DisableShuffleCheck()
 		if Duel.DiscardDeck(tp,6,REASON_EFFECT) then
 		Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,e:GetHandler())
 		Duel.ShuffleHand(tp)
 	end
-	local ge1=Effect.CreateEffect(e:GetHandler())
-	ge1:SetType(EFFECT_TYPE_FIELD)
-	ge1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	ge1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	ge1:SetTargetRange(1,0)
-	ge1:SetTarget(s.splimit)
-	ge1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(ge1,tp)
-	aux.RegisterClientHint(e:GetHandler(),EFFECT_FLAG_OATH,tp,1,0,aux.Stringid(id,0),nil)
-end
-function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return c:IsLocation(LOCATION_EXTRA)
 end
