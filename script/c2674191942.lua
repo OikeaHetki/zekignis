f0a967aacd2fbef7d768af802a8463bf0bb012c7
OikeaHetki <zakktrez@gmail.com>
2022-09-09 07:59:19 -0400

    test

--- script/c2674191942.lua
+++ script/c2674191942.lua
@@ -14,37 +14,17 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then 
-		local g=Duel.GetMatchingGroup(aux.NOT(Card.IsPublic),tp,LOCATION_DECK,0,nil)
-			return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_DECK,0,3,nil) 
-			and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>2
-			and g:GetClassCount(Card.GetCode)>=3 end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_DECK,0,5,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
-	local g=Duel.GetMatchingGroup(aux.NOT(Card.IsPublic),tp,LOCATION_DECK,0,nil)
-	if g:GetClassCount(Card.GetCode)<3 then return false end
-	local sg=aux.SelectUnselectGroup(g,e,tp,3,3,aux.dncheck,1,tp,HINTMSG_CONFIRM)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_DECK,0,5,5,nil)
+	if #g<5 then return end
 	Duel.ConfirmCards(1-tp,g)
 	Duel.Hint(HINT_SELECTMSG,1-tp,aux.Stringid(id,1))
 	local sg=g:Select(1-tp,1,1,nil)
 			Duel.ShuffleDeck(tp)
 			Duel.MoveSequence(sg,0)
 			Duel.ConfirmDecktop(tp,1)
-	g:Sub(sg)
-	Duel.SendtoGrave(g,REASON_RULE)
-	-- halve battle damage
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CHANGE_DAMAGE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(0,1)
-	e1:SetValue(s.damval)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,2),nil)
-end
-function s.damval(e,re,val,r,rp,rc)
-	return math.floor(val/2)
 end
