38f5e41a9e93a78bcecaaf965a24c9e17bddd639
OikeaHetki <zakktrez@gmail.com>
2022-09-09 07:30:24 -0400

    test

--- script/c2642829885.lua
+++ script/c2642829885.lua
@@ -5,7 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
