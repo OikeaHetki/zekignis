763607e4ace40cdc316a6ec30d8d8c8dde439712
OikeaHetki <zakktrez@gmail.com>
2022-09-15 09:58:01 -0400

    test

--- script/c2690006556.lua
+++ script/c2690006556.lua
@@ -6,6 +6,7 @@ function s.initial_effect(c)
 	c:SetUniqueOnField(1,0,id)
 	--special summon [Dash 1R]
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
@@ -14,24 +15,23 @@ function s.initial_effect(c)
 	e1:SetCondition(s.spcon)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--to hand [Dash EOT Effect]
-
 	--Effect on combat damage
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_DECKDES+CATEGORY_TOKEN+CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_BATTLE_DAMAGE)
-	e3:SetCondition(s.ddcon)
-	e3:SetTarget(s.ddtg)
-	e3:SetOperation(s.ddop)
-	c:RegisterEffect(e3)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DECKDES+CATEGORY_TOKEN+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_BATTLE_DAMAGE)
+	e2:SetCondition(s.ddcon)
+	e2:SetTarget(s.ddtg)
+	e2:SetOperation(s.ddop)
+	c:RegisterEffect(e2)
 	--direct
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_DIRECT_ATTACK)
-	c:RegisterEffect(e4)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_DIRECT_ATTACK)
+	c:RegisterEffect(e3)
 end
+--Dash 1R
 function s.spcon(e,c)
 	if c==nil then return true end
 	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
@@ -49,7 +49,7 @@ end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
     Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
 end
----effect on combat
+---Combat Damage
 function s.ddcon(e,tp,eg,ep,ev,re,r,rp)
 	return ep~=tp
 end
@@ -60,10 +60,34 @@ function s.ddtg(e,tp,eg,ep,ev,re,r,rp,chk)
 		return tc and tc:IsAbleToRemove()
 	end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
+end
+function s.treasfil(c)
+	return c:IsFaceup() and c:IsCode(id+1)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsCode(id+1)
 end
 function s.ddop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetDecktopGroup(1-tp,1)
 	Duel.DisableShuffleCheck()
-	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	if Duel.Remove(g,POS_FACEUP,REASON_EFFECT) and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,2000,4,RACE_ROCK,ATTRIBUTE_LIGHT) then 		
+	local token=Duel.CreateToken(tp,id+1)
+	Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+	local rg=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_REMOVED,nil,e,tp)
+	if #rg>0 and Duel.IsExistingMatchingCard(s.treasfil,tp,LOCATION_MZONE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then 
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3))
+			local sg=rg:Select(tp,1,1,nil)
+			local sc=sg:GetFirst()
+			local sc:IsAbleToHand()
+				Duel.SendtoHand(sc,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,sc)
+			end
+			Duel.ShuffleHand(tp)
+	else
+	Duel.SendtoGrave(tc,REASON_EFFECT+REASON_REVEAL)
 end
