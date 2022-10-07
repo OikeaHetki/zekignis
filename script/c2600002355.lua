2eac7cc04316f968d2ef1b4abd82b529827a044e
OikeaHetki <zakktrez@gmail.com>
2022-10-07 12:53:43 -0400

    test

--- script/c2600002355.lua
+++ script/c2600002355.lua
@@ -14,6 +14,7 @@ function s.initial_effect(c)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
 	e1:SetCountLimit(1,id)
 	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
 end
