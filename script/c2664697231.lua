b5e4b0b0adc2f56ded10b61d6835f9c8dbb66d7d
OikeaHetki <zakktrez@gmail.com>
2022-12-30 11:56:22 -0500

    field fix

---
+++ script/c2664697231.lua
@@ -0,0 +1,44 @@
+--ダスト・シュート
+--Trap Dustshoot
+--zekpro version
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetHintTiming(0,TIMING_TOHAND)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>3
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local g=Duel.GetFieldGroup(p,0,LOCATION_HAND)
+	if #g>0 then
+		Duel.ConfirmCards(p,g)
+		local tg=g:Filter(Card.IsMonster,nil)
+		if #tg>0 then
+			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
+			local sg=tg:Select(p,1,1,nil)
+			Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
+		end
+		Duel.ShuffleHand(1-p)
+	end
+end
