b7237cee012e5113b83436778d70477d4bacd2bc
OikeaHetki <zakktrez@gmail.com>
2022-12-31 19:06:46 -0500

    fix to equip spells so that they go to top deck instead of hand

--- script/c2655321970.lua
+++ script/c2655321970.lua
@@ -59,7 +59,7 @@ function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.SendtoHand(e:GetHandler(),nil,0,REASON_EFFECT)
+		Duel.SendtoDeck(e:GetHandler(),nil,0,REASON_EFFECT)
 	end
 end
 --destroyreplace
