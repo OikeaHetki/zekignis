a29d71a80b650b0a39228f3badda4857580dbea8
OikeaHetki <zakktrez@gmail.com>
2023-01-06 08:51:01 -0500

    est

--- script/c2683555666.lua
+++ script/c2683555666.lua
@@ -28,7 +28,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() and Duel.Destroy(tc,REASON_EFFECT)~=0 then
 		local atk=tc:GetAttack()
 		if atk<0 then atk=0 end
 		local val=Duel.Damage(tp,atk,REASON_EFFECT)
