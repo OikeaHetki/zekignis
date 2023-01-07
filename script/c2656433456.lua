12e927cc0825efd401539f1196fc13af030c9f2e
OikeaHetki <zakktrez@gmail.com>
2023-01-07 10:08:14 -0500

    ssits

--- script/c2656433456.lua
+++ script/c2656433456.lua
@@ -18,51 +18,26 @@ function s.initial_effect(c)
 	e2:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_FAIRY))
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
-	--Gain 500 LP
+	--recover 1
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_RECOVER)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_FZONE)
-	e3:SetCode(EVENT_BATTLE_DESTROYING)
-	e3:SetProperty(EFFECT_FLAG_BOTH_SIDE+EFFECT_FLAG_DELAY)
-	e3:SetCondition(s.reccon)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_BOTH_SIDE)
+	e3:SetCountLimit(1)
 	e3:SetTarget(s.rectg)
 	e3:SetOperation(s.recop)
 	c:RegisterEffect(e3)
-	local e4=Effect.CreateEffect(c)
-	e4:SetCategory(CATEGORY_RECOVER)
-	e4:SetDescription(aux.Stringid(id,2))
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e4:SetProperty(EFFECT_FLAG_BOTH_SIDE+EFFECT_FLAG_DELAY)
-	e4:SetRange(LOCATION_FZONE)
-	e4:SetCode(EVENT_CHAIN_NEGATED)
-	e4:SetCondition(s.negcon)
-	e4:SetTarget(s.negtg)
-	e4:SetOperation(s.recop)
-	c:RegisterEffect(e4)
-end
-function s.reccon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local rc=eg:GetFirst()
-	return rc:IsRelateToBattle() and rc:IsRace(RACE_FAIRY) and rc:IsFaceup() and rc:IsControler(tp)
 end
 function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
+	local val=Duel.GetMatchingGroupCount(Card.IsRace,tp,LOCATION_MZONE,0,nil,RACE_FAIRY)*200
 	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(200)
-	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,200)
+	Duel.SetTargetParam(val)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,val)
 end
 function s.recop(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Recover(p,d,REASON_EFFECT)
-end
-function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp
-end
-function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not e:GetHandler():IsStatus(STATUS_CHAINING)
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(200)
-	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,200)
+	local val=Duel.GetMatchingGroupCount(Card.IsRace,tp,LOCATION_MZONE,0,nil,RACE_FAIRY)*200
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.Recover(p,val,REASON_EFFECT)
 end
