a103b1b6772d90d6442988313352ef07d68158bc
OikeaHetki <zakktrez@gmail.com>
2023-01-18 10:15:43 -0500

    test

--- script/c2629401950.lua
+++ script/c2629401950.lua
@@ -16,7 +16,7 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 	local e3=e1:Clone()
-	e3:SetCode(EVENT_SPECIAL_SUMMON_SUCCESS)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
