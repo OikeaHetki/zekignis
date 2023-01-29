e154f4209597503be02756a686e24bac920d7ebc
OikeaHetki <zakktrez@gmail.com>
2023-01-29 05:43:06 -0500

    nex

--- script/c2681913510.lua
+++ script/c2681913510.lua
@@ -3,18 +3,18 @@
 --zekpro version
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--spsummon
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	--to hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_GRAVE)
@@ -25,33 +25,45 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 s.listed_series={0x1f}
+s.list={[17955766]=2678734254,[43237273]=2613857935,[54959865]=2678734259,[17732278]=2613857930,[89621922]=2613857940,[80344569]=2678734264}
+--Tribute 1 to summon the other
 function s.filter1(c,e,tp)
-	local race=c:GetOriginalRace()
-	return c:IsFaceup() and c:IsSetCard(0x1f)
-		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,code,e,tp,c)
+	if c:IsFacedown() then return false end
+	local code=c:GetCode()
+	local tcode=s.list[code]
+	return tcode and Duel.IsExistingTarget(s.filter2,tp,LOCATION_EXTRA,0,1,nil,tcode,e,tp)
 end
-function s.filter2(c,code,e,tp,mc)
-	return c:IsRace(race) and c:GetLevel()==4 
-	and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+function s.filter2(c,tcode,e,tp)
+	return c:IsCode(tcode) and c:IsCanBeSpecialSummoned(e,0,tp,true,false) and c:GetLevel()==4
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(1)
+	return true
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE+LOCATION_HAND) and chkc:IsControler(tp) and s.filter1(chkc,e,tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE+LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE+LOCATION_HAND,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	if chkc then return false end
+	if chk==0 then
+		local res=e:GetLabel()==1
+		e:SetLabel(0)
+		return res and Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+			and Duel.CheckReleaseGroupCost(tp,s.filter1,1,false,aux.ReleaseCheckMMZ,nil,e,tp) end
+	e:SetLabel(0)
+	local rg=Duel.SelectReleaseGroupCost(tp,s.filter1,1,1,false,aux.ReleaseCheckMMZ,nil,e,tp)
+	local code=rg:GetFirst():GetCode()
+	local tcode=s.list[code]
+	Duel.Release(rg,REASON_COST)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc1=Duel.GetFirstTarget()
-	if tc1 and tc1:IsRelateToEffect(e) and Duel.SendtoGrave(tc1,REASON_EFFECT)~=0 and tc1:IsLocation(LOCATION_GRAVE) then
-		local race=tc1:GetOriginalRace()
-		local tc2=Duel.GetFirstMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,nil,race,e,tp,tc1)
-		if tc2 and Duel.SpecialSummon(tc2,0,tp,tp,true,false,POS_FACEUP)~=0 then
-			tc2:CompleteProcedure()
-		end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil,tcode,e,tp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
+--addbacktohand
 function s.thfilter(c,e)
 	return c:IsSetCard(0x9) and c:IsType(TYPE_FUSION)
 end
