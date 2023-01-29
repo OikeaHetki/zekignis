2f1d4cedd0b3a537adb2bc481cf978f3bc18330d
OikeaHetki <zakktrez@gmail.com>
2023-01-29 14:33:26 -0500

    test

--- script/c2600062121.lua
+++ script/c2600062121.lua
@@ -23,6 +23,7 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1)
+	e3:SetCondition(s.condition)
 	e3:SetTarget(s.target)
 	e3:SetOperation(s.activate)
 	c:RegisterEffect(e3)
@@ -58,36 +59,37 @@ function s.poop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 --castle of dark illusions set
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_MAIN2
+end
 function s.filter(c)
-	return c:IsFaceup() and c:IsCanTurnSet() and not c:IsType(TYPE_EFFECT)
+	return c:IsCanChangePosition() and c:IsFaceup() and not (c:IsCode(id) or c:IsOriginalCode(id)) and c:IsCanTurnSet()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.ChangePosition(tc,POS_FACEDOWN_ATTACK,0,POS_FACEDOWN_DEFENSE,0)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MMZONE,0,1,nil)
 	end
 end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MMZONE,0,nil)
+	Duel.ChangePosition(g,POS_FACEDOWN_ATTACK,0) 
+end
 --shuffle facedowns
+function s.fffilter(c)
+	return c:IsPosition(POS_FACEDOWN_ATTACK)
+end
 function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFacedown,tp,LOCATION_MMZONE,0,2,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.fffilter,tp,LOCATION_MMZONE,0,2,nil) end
 end
 function s.posop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local g=Duel.GetMatchingGroup(Card.IsFacedown,tp,LOCATION_MMZONE,0,nil)
+	local g=Duel.GetMatchingGroup(s.fffilter,tp,LOCATION_MMZONE,0,nil)
 	Duel.ShuffleSetCard(g)
-		Duel.ChangePosition(tc,POS_FACEDOWN_ATTACK)
 end
 --cannot attack
-function s.filter(c)
+function s.ffilter(c)
 	return c:IsFacedown()
 end
 function s.cbbtcon(e)
-	return Duel.IsExistingMatchingCard(s.filter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(s.ffilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
