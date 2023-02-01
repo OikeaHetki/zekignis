201a81132c9e7e4149c48a8e1b90eba9ba139244
OikeaHetki <zakktrez@gmail.com>
2023-02-01 09:26:54 -0500

    test

--- script/c2644910027.lua
+++ script/c2644910027.lua
@@ -37,7 +37,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			e1:SetCode(EFFECT_SET_ATTACK)
+			e1:SetCode(EFFECT_SET_BASE_ATTACK)
 			e1:SetValue(10000)
 			c:RegisterEffect(e1)
 	end
