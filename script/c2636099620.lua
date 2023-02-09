743b2b5a6ae0fed13242d713028cf17cf536744e
OikeaHetki <zakktrez@gmail.com>
2023-02-09 09:39:15 -0500

    necroface, lightsworn field spell, and some other things

---
+++ script/c2636099620.lua
@@ -0,0 +1,81 @@
+--ジャスティス・ワールド
+--Realm of Light
+--zekpro version
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableCounterPermit(0x5)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Add counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetOperation(s.acop)
+	c:RegisterEffect(e2)
+	--atkup
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x38))
+	e3:SetValue(s.atkval)
+	c:RegisterEffect(e3)
+	--Destroy replace
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_DESTROY_REPLACE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetTarget(s.desreptg)
+	e4:SetOperation(s.desrepop)
+	c:RegisterEffect(e4)
+	--Set itself
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_TO_GRAVE)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetCondition(s.condition)
+	e5:SetTarget(s.target)
+	e5:SetOperation(s.operation)
+	c:RegisterEffect(e5)
+end
+s.listed_series={0x38}
+s.counter_place_list={0x5}
+function s.atkval(e,c)
+	return e:GetHandler():GetCounter(0x5)*100
+end
+function s.cfilter(c,tp)
+	return c:GetPreviousLocation()==LOCATION_DECK
+end
+function s.acop(e,tp,eg,ep,ev,re,r,rp)
+	if eg:IsExists(s.cfilter,1,nil,tp) then
+		e:GetHandler():AddCounter(0x5,1)
+	end
+end
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not e:GetHandler():IsReason(REASON_REPLACE+REASON_RULE)
+		and e:GetHandler():GetCounter(0x5)>=2 end
+	return true
+end
+function s.desrepop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RemoveCounter(ep,0x5,2,REASON_EFFECT)
+end
+--set
+function s.condtion(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetPreviousLocation()==LOCATION_DECK
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsSSetable() end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsSSetable() then Duel.SSet(tp,c)
+	end
+end
