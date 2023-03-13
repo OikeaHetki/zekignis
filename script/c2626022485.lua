4c09bab324d53d1fbbac7883e7767a0199b885d7
OikeaHetki <zakktrez@gmail.com>
2023-03-13 14:41:11 -0400

    update to banlists

--- script/c2626022485.lua
+++ script/c2626022485.lua
@@ -5,8 +5,10 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCode(EVENT_DRAW)
+	e1:SetCondition(s.condition)
 	c:RegisterEffect(e1)
 	--
 	local e2=Effect.CreateEffect(c)
@@ -15,19 +17,9 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(0,1)
-	e2:SetValue(3)
+	e2:SetValue(2)
 	c:RegisterEffect(e2)
-	--draw
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_DRAW)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetOperation(s.drop)
-	c:RegisterEffect(e3)
 end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	if ep~=tp then
-		Duel.Recover(tp,300,REASON_EFFECT)
-		Duel.Damage(1-tp,200,REASON_EFFECT)
-	end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and Duel.GetCurrentPhase()~=PHASE_DRAW
 end
