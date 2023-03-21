604be802188f6f57664fdf1b4a6d3f44df78a49b
OikeaHetki <zakktrez@gmail.com>
2023-03-21 07:47:58 -0400

    test

--- script/c2615310033.lua
+++ script/c2615310033.lua
@@ -11,12 +11,32 @@ function s.initial_effect(c)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0,nil)==0
-		and	Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE,nil)>0
+		and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE,nil)>0
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	--Banish it if it leaves the field
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(3300)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+	e1:SetValue(LOCATION_REMOVED)
+	e:GetHandler():RegisterEffect(e1,true)
 end
