fe25df0d6a1ef727c5467571edc78e5389a69c2d
OikeaHetki <zakktrez@gmail.com>
2023-04-03 18:03:15 -0400

    test

--- script/c2600000750.lua
+++ script/c2600000750.lua
@@ -22,7 +22,7 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,Duel.GetLocationCount(tp,LOCATION_MZONE))
 end
 function s.spfilter(c,e,tp,g)
-	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and (not g or not g:IsContains(c)) and c:IsSetCard(0x18) and c:IsLevelBelow(4)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,true) and (not g or not g:IsContains(c)) and c:IsSetCard(0x18) and c:IsLevelBelow(4)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
@@ -31,15 +31,15 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetTargetCard(g)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,g,1,0x1019,1)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetTargetCards(e)
 	if #g==0 then return end
 	Duel.SendtoHand(g,nil,REASON_EFFECT)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp,g)
-	if sg:IsRelateToEffect(e) then 
-	Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_ATTACK) 
-	sg:AddCounter(0x1019,1)
+	local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp,g):GetFirst()
+	if sg and Duel.SpecialSummon(sg,0,tp,tp,false,true,POS_FACEUP_ATTACK)
+	and sg:IsFaceup() then sg:AddCounter(0x1019,1)
 	end
 end
