824b612c92b22623788b81bc1a2358cd1d1a7a17
OikeaHetki <zakktrez@gmail.com>
2023-10-06 07:20:07 -0400

    test

--- script/c2600001140.lua
+++ script/c2600001140.lua
@@ -1,4 +1,4 @@
---G - 暗黒破	
+--G - 暗黒破   
 --Golgari Darkblast
 --zek
 local s,id=GetID()
@@ -12,76 +12,76 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-    --Replace your normal draw with dredge 3
-    local e5=Effect.CreateEffect(c)
-    e5:SetDescription(aux.Stringid(id,1))
-    e5:SetCategory(CATEGORY_DECKDES+CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e5:SetCode(EVENT_PREDRAW)
-    e5:SetRange(LOCATION_GRAVE)
-    e5:SetCondition(s.dredgecon)
-    e5:SetTarget(s.dredgetg)
-    e5:SetOperation(s.dredgeop)
-    c:RegisterEffect(e5)
-    --verify the starting deck possesses 45 cards
-    aux.GlobalCheck(s,function()
-    local ge1=Effect.CreateEffect(c)
-    ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-    ge1:SetCode(EVENT_STARTUP)
-    ge1:SetOperation(s.checkop)
-    Duel.RegisterEffect(ge1,0) end)
+	--Replace your normal draw with dredge 3
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_DECKDES+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_PREDRAW)
+	e5:SetRange(LOCATION_GRAVE)
+	e5:SetCondition(s.dredgecon)
+	e5:SetTarget(s.dredgetg)
+	e5:SetOperation(s.dredgeop)
+	c:RegisterEffect(e5)
+	--verify the starting deck possesses 45 cards
+	aux.GlobalCheck(s,function()
+	local ge1=Effect.CreateEffect(c)
+	ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	ge1:SetCode(EVENT_STARTUP)
+	ge1:SetOperation(s.checkop)
+	Duel.RegisterEffect(ge1,0) end)
 end
 --deckcheck
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-    local ct1=Duel.GetFieldGroupCount(0,LOCATION_DECK,0)
-    Duel.RegisterFlagEffect(0,id,0,0,1,ct1)
-    local ct2=Duel.GetFieldGroupCount(1,LOCATION_DECK,0)
-    Duel.RegisterFlagEffect(1,id,0,0,1,ct2)
+	local ct1=Duel.GetFieldGroupCount(0,LOCATION_DECK,0)
+	Duel.RegisterFlagEffect(0,id,0,0,1,ct1)
+	local ct2=Duel.GetFieldGroupCount(1,LOCATION_DECK,0)
+	Duel.RegisterFlagEffect(1,id,0,0,1,ct2)
 end
 ---dredge 3
 function s.dredgecon(e,tp,eg,ep,ev,re,r,rp)
-    return tp==Duel.GetTurnPlayer() and Duel.GetDrawCount(tp)>0
-        and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>2
-            and Duel.GetFlagEffectLabel(tp,id)>=45
+	return tp==Duel.GetTurnPlayer() and Duel.GetDrawCount(tp)>0
+		and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>2
+			and Duel.GetFlagEffectLabel(tp,id)>=45
 end
 function s.dredgetg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():IsAbleToHand() end
-    local dt=Duel.GetDrawCount(tp)
-    if dt~=0 then
-        _replace_count=0
-        _replace_max=dt
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_FIELD)
-        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-        e1:SetCode(EFFECT_DRAW_COUNT)
-        e1:SetTargetRange(1,0)
-        e1:SetReset(RESET_PHASE+PHASE_DRAW)
-        e1:SetValue(0)
-        Duel.RegisterEffect(e1,tp)
-    end
-    Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
-    Duel.SetChainLimit(aux.FALSE)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	local dt=Duel.GetDrawCount(tp)
+	if dt~=0 then
+		_replace_count=0
+		_replace_max=dt
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetCode(EFFECT_DRAW_COUNT)
+		e1:SetTargetRange(1,0)
+		e1:SetReset(RESET_PHASE+PHASE_DRAW)
+		e1:SetValue(0)
+		Duel.RegisterEffect(e1,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+	Duel.SetChainLimit(aux.FALSE)
 end
 function s.dredgeop(e,tp,eg,ep,ev,re,r,rp)
-    if not e:GetHandler():IsRelateToEffect(e) then return end
-    if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<=2 then return end
-        Duel.DisableShuffleCheck()
-        if Duel.DiscardDeck(tp,3,REASON_EFFECT) then
-        Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,e:GetHandler())
-        Duel.ShuffleHand(tp)
-    end
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<=2 then return end
+		Duel.DisableShuffleCheck()
+		if Duel.DiscardDeck(tp,3,REASON_EFFECT) then
+		Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,e:GetHandler())
+		Duel.ShuffleHand(tp)
+	end
 end
 --destroy
 function s.filter(c)
 	return c:IsFaceup() and c:IsAttackBelow(1000) and c:IsDefenseBelow(1000)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
---
+++ script/c2600001190.lua
@@ -0,0 +1,68 @@
+--
+--Golgari Necroplasm
+--zek
+local s,id=GetID()
+function s.initial_effect(c)
+	--atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.atkcon)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+	--destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.descon)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+end
+--atkup
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return tp==Duel.GetTurnPlayer()
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	e1:SetValue(500)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	e2:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
+end
+--des
+function s.desfilter(c,atk)
+	return c:IsFaceup() and c:GetAttack()<=atk
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectTarget(tp,s.desfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) 
+		then Duel.Destroy(tc,REASON_EFFECT)>0 
+	end
+end
