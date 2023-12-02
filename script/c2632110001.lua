add1e15b354bdacb3fe23a1c0398f81da832cb1b
OikeaHetki <zakktrez@gmail.com>
2023-12-02 11:32:06 -0500

    test

--- script/c2632110001.lua
+++ script/c2632110001.lua
@@ -7,7 +7,6 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
@@ -27,6 +26,17 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.ConfirmCards(1-tp,g)
 	Duel.ShuffleHand(tp)
 	e:SetLabel(g:GetFirst():GetAttribute())
+	-- Cannot special summon more than once
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetOwnerPlayer(tp)
+	e1:SetCondition(s.spcon)
+	e1:SetTargetRange(1,0)
+	Duel.RegisterEffect(e1,tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
@@ -49,4 +59,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			local sc=Duel.SelectMatchingCard(tp,s.sumfilter,tp,LOCATION_HAND,0,1,1,tp):GetFirst()
 			Duel.Summon(tp,sc,true,nil) 
 	end
+end
+function s.spcon(e)
+	return Duel.GetFlagEffect(e:GetOwnerPlayer(),id)>0
 end
