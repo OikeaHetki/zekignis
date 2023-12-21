b2b0436d8487e90e6c6cbdc1be6dbbaf3e625281
OikeaHetki <zakktrez@gmail.com>
2023-12-20 20:39:44 -0500

    cleaning up goat format

--- script/c2677581312.lua
+++ script/c2677581312.lua
@@ -1,3 +1,4 @@
+--かめんどうけ 2
 --Masked Clown #2
 --zek
 local s,id=GetID()
@@ -5,10 +6,11 @@ function s.initial_effect(c)
 	--destroy
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_POSITION)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BATTLE_START)
 	e1:SetCondition(s.descon)
+	e1:SetCost(s.descost)
 	e1:SetTarget(s.destg)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
@@ -17,17 +19,20 @@ function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local d=Duel.GetAttackTarget()
 	return e:GetHandler()==Duel.GetAttacker() and d and d:IsDefensePos()
 end
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chkc then return false end
-	local bc=Duel.GetAttackTarget()
-	if chk==0 then return bc and bc:IsCanBeEffectTarget(e) end
-	Duel.SetTargetCard(bc)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,bc,1,0,0)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,Duel.GetAttackTarget(),1,0,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_POSITION,e:GetHandler(),1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and Duel.Damage(tp,1000,REASON_EFFECT) and Duel.Destroy(tc,REASON_EFFECT) then
-	Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+	local d=Duel.GetAttackTarget()
+	if d:IsRelateToBattle() and
+		Duel.Destroy(d,REASON_EFFECT)
+			then Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
 	end
 end
