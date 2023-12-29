82f3eccb6da83029d44ff11bf2492e77d1a6267b
OikeaHetki <zakktrez@gmail.com>
2023-12-29 10:13:09 -0500

    tsc test

--- script/c2600000096.lua
+++ script/c2600000096.lua
@@ -4,13 +4,13 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	local e1=Fusion.CreateSummonEff(c,nil,Fusion.OnFieldMat(Card.IsAbleToDeck),s.fextra,Fusion.ShuffleMaterial,nil,s.stage2)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	--e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetCost(s.cost)
 	c:RegisterEffect(e1)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckLPCost(tp,2000) end
-	Duel.PayLPCost(tp,2000)
+	if chk==0 then return Duel.CheckLPCost(tp,3000) end
+	Duel.PayLPCost(tp,3000)
 end
 function s.fextra(e,tp,mg)
 	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(aux.AND(Card.IsAbleToDeck,Card.IsFaceup)),tp,LOCATION_REMOVED,0,nil)
@@ -25,21 +25,8 @@ function s.stage2(e,tc,tp,sg,chk)
 		e1:SetTarget(s.atktg)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
-		local e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e2:SetCode(EVENT_ADJUST)
-		e2:SetRange(LOCATION_MZONE)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e2:SetOperation(s.op)
-		tc:RegisterEffect(e2)
 	end
 end
 function s.atktg(e,c)
 	return e:GetLabel()~=c:GetFieldID()
-end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
-	local bc=e:GetHandler():GetBattleTarget()
-	if bc and bc:IsStatus(STATUS_BATTLE_DESTROYED)
-		then Duel.Remove(bc,bc:GetPosition(),REASON_EFFECT)
-	end
 end
