1d450be6ad05ddc0c400a76756ab52ccd35956e8
OikeaHetki <zakktrez@gmail.com>
2024-01-05 20:46:37 -0500

    buff to snatch; no longer gives lp and you can have more than one

--- script/c2645986603.lua
+++ script/c2645986603.lua
@@ -3,13 +3,14 @@
 --zekpro version
 local s,id=GetID()
 function s.initial_effect(c)
-	c:SetUniqueOnField(1,0,45986603)
+	--c:SetUniqueOnField(1,0,45986603)
 	aux.AddEquipProcedure(c,1,aux.CheckStealEquip,s.eqlimit,nil,s.target)
 	--recover
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCategory(CATEGORY_DRAW+CATEGORY_RECOVER)
+	e1:SetCategory(CATEGORY_DRAW)
+	--e1:SetCategory(CATEGORY_DRAW+CATEGORY_RECOVER)
 	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetCountLimit(1)
@@ -64,14 +65,14 @@ function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(1)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,1-tp,1)
-	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,1-tp,1000)
+	--Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,1-tp,1)
+	--Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,1-tp,1000)
 end
 function s.recop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
-	Duel.Recover(p,1000,REASON_EFFECT)
+	--Duel.Recover(p,1000,REASON_EFFECT)
 end
 function s.ctval(e,c)
 	return e:GetHandlerPlayer()
