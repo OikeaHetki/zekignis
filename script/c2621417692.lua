3514ac33eddf2b7c2f833250361f1b3ef266eaed
OikeaHetki <zakktrez@gmail.com>
2024-03-05 10:52:46 -0500

    more goat big stat creature erratas

--- script/c2621417692.lua
+++ script/c2621417692.lua
@@ -3,20 +3,34 @@
 --zekpro version
 local s,id=GetID()
 function s.initial_effect(c)
-	--attack cost
+	--self destroy
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_ATTACK_COST)
-	e1:SetCost(s.atcost)
-	e1:SetOperation(s.atop)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_SELF_DESTROY)
+	e1:SetCondition(s.sdcon)
 	c:RegisterEffect(e1)
+	--self destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EVENT_DAMAGE_STEP_END)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(s.sdop)
+	c:RegisterEffect(e2)
 end
-function s.atcost(e,c,tp)
-	return Duel.CheckLPCost(tp,400)
+function s.sdfilter(c)
+	return c:IsFaceup() and c:GetAttribute()~=ATTRIBUTE_DARK
 end
-function s.atop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.IsAttackCostPaid()~=2 and e:GetHandler():IsLocation(LOCATION_MZONE) then
-		Duel.PayLPCost(tp,400)
-		Duel.AttackCostPaid()
+function s.sdcon(e)
+	return Duel.IsExistingMatchingCard(s.sdfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	if bc:IsRelateToBattle() and bc:IsFaceup() and bc:IsAttribute(ATTRIBUTE_LIGHT) then
+		Duel.Destroy(c,REASON_EFFECT)
 	end
 end
