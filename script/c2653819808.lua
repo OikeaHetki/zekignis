054f67283d1c270ced5d8aad2c621e68e348b3f0
OikeaHetki <zakktrez@gmail.com>
2024-05-06 20:31:21 -0400

    first push on new device to remove starlight road errata

--- script/c2653819808.lua
+++ script/c2653819808.lua
@@ -70,5 +70,6 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
 	end
 end
