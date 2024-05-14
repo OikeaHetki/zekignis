b34e048a7cdbe6e7f1f805ed529662f7cfb3d5a9
OikeaHetki <zakktrez@gmail.com>
2024-05-14 15:08:30 -0400

    lsits

--- script/c2685742772.lua
+++ script/c2685742772.lua
@@ -11,12 +11,16 @@ function s.initial_effect(c)
 	--cannot attack
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(s.atktarget)
+	e2:SetTarget(s.tg)
 	c:RegisterEffect(e2)
+	--pos limit
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+	c:RegisterEffect(e3)
 end
-function s.atktarget(e,c)
-	return c:GetLevel()>=5 or c:GetRank()>=4 or c:IsLinkAbove(3)
+function s.tg(e,c)
+	return c:GetAttack()>=1500
 end
