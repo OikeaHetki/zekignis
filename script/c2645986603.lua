303b52b4989e36102ecbecb1fdb3150e55e33704
OikeaHetki <zekthesnek@gmail.com>
2025-02-02 19:45:41 -0500

    test

--- script/c2645986603.lua
+++ script/c2645986603.lua
@@ -1,30 +1,34 @@
 --強奪
 --Snatch Steal
---zekpro version
+--zekpro version (monster effect cannot activate)
 local s,id=GetID()
 function s.initial_effect(c)
-	--c:SetUniqueOnField(1,0,45986603)
 	aux.AddEquipProcedure(c,1,aux.CheckStealEquip,s.eqlimit,nil,s.target)
 	--recover
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCategory(CATEGORY_RECOVER)
-	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e1:SetRange(LOCATION_SZONE)
-	e1:SetCountLimit(1)
-	e1:SetCondition(s.reccon)
-	e1:SetTarget(s.rectg)
-	e1:SetOperation(s.recop)
-	c:RegisterEffect(e1)
-	--inactivate
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCategory(CATEGORY_RECOVER)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetType(EFFECT_TYPE_EQUIP)
-	e2:SetCode(EFFECT_CANNOT_TRIGGER)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.reccon)
+	e2:SetTarget(s.rectg)
+	e2:SetOperation(s.recop)
 	c:RegisterEffect(e2)
+	--inactivate
+	local e3=Effect.CreateEffect(c)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetCode(EFFECT_CANNOT_TRIGGER)
+	c:RegisterEffect(e3)
+	--control
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_EQUIP)
+	e4:SetCode(EFFECT_SET_CONTROL)
+	e4:SetValue(s.ctval)
+	c:RegisterEffect(e4)
 end
---control; equip functions
 function s.eqlimit(e,c)
 	return e:GetHandlerPlayer()~=c:GetControler() or e:GetHandler():GetEquipTarget()==c
 end
@@ -32,9 +36,8 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,tc)
 	e:SetCategory(CATEGORY_CONTROL+CATEGORY_EQUIP)
 	Duel.SetOperationInfo(0,CATEGORY_CONTROL,tc,1,0,0)
 end
---recover LP
 function s.reccon(e,tp,eg,ep,ev,re,r,rp)
-  return tp~=Duel.GetTurnPlayer()
+	return tp~=Duel.GetTurnPlayer()
 end
 function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
@@ -46,4 +49,7 @@ function s.recop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Recover(p,d,REASON_EFFECT)
+end
+function s.ctval(e,c)
+	return e:GetHandlerPlayer()
 end
