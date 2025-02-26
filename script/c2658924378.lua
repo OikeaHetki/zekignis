9ac6dd1733df180b3c395655e36b03edd6b5106a
OikeaHetki <zekthesnek@gmail.com>
2025-02-26 09:34:55 -0500

    wattcastle

--- script/c2658924378.lua
+++ script/c2658924378.lua
@@ -1,6 +1,6 @@
 --エレキャッスル
 --Wattcastle
---zekpro version (new effects)
+--zekpro version (does not affected Thunder)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -15,29 +15,6 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetOperation(s.atkdn)
 	c:RegisterEffect(e2)
-	--search
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_FZONE)
-	e3:SetProperty(EFFECT_FLAG_BOTH_SIDE)
-	e3:SetCountLimit(1)
-	e3:SetCost(s.thcost)
-	e3:SetTarget(s.thtg)
-	e3:SetOperation(s.thop)
-	c:RegisterEffect(e3)
-	--add 1 Thunder monster from GY to hand
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCategory(CATEGORY_TOHAND)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-	e4:SetCode(EVENT_DESTROYED)
-	e4:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_FZONE) end)
-	e4:SetTarget(s.thtg2)
-	e4:SetOperation(s.thop2)
-	c:RegisterEffect(e4)
 end
 s.listed_series={0xe}
 function s.atkdn(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -50,41 +27,35 @@ function s.atkdn(e,tp,eg,ep,ev,re,r,rp,chk)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		e1:SetValue(-1000)
 		a:RegisterEffect(e1)
-	end
-end
-function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
-	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
-end
-function s.thfilter(c)
-	return c:IsSetCard(0xe) and c:IsSpellTrap()  and c:IsAbleToHand() and (not c:IsCode(id) or c:IsOriginalCode(id))
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
-end
-function s.thfilter2(c)
-	return c:IsMonster() and c:IsRace(RACE_THUNDER) and c:IsAbleToHand()
-end
-function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter2(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.thfilter2,tp,LOCATION_GRAVE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectTarget(tp,s.thfilter2,tp,LOCATION_GRAVE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-end
-function s.thop2(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e2:SetValue(-1000)
+		a:RegisterEffect(e2)
+			--effects negated
+			local e3=Effect.CreateEffect(e:GetHandler())
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_DISABLE)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+			a:RegisterEffect(e3)
+			local e4=Effect.CreateEffect(e:GetHandler())
+			e4:SetType(EFFECT_TYPE_SINGLE)
+			e4:SetCode(EFFECT_DISABLE_EFFECT)
+			e4:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+			a:RegisterEffect(e4)
+			--Cannot change its battle position
+			local e5=Effect.CreateEffect(e:GetHandler())
+			e5:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+			e5:SetType(EFFECT_TYPE_SINGLE)
+			e5:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+			e5:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e5)
+			--Cannot attack
+			local e6=Effect.CreateEffect(e:GetHandler())
+			e6:SetType(EFFECT_TYPE_SINGLE)
+			e6:SetCode(EFFECT_CANNOT_ATTACK)
+			e6:SetReset(reset)
+			tc:RegisterEffect(e6)
 	end
 end
