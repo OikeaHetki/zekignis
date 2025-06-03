994d7e99856c72cea3c111570e3e5fd409b1981f
OikeaHetki <zekthesnek@gmail.com>
2025-06-03 10:09:01 -0400

    fabled nehemoth negotiations with nes have lead to this update

--- script/c2600000123.lua
+++ script/c2600000123.lua
@@ -4,7 +4,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--synchro summon
-	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x35),1,1,Synchro.NonTuner(nil),2,99)
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x35),1,1,Synchro.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
 	--special summon
 	local e1=Effect.CreateEffect(c)
