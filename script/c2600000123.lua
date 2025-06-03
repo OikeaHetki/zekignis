02db5d50a03fe67d6035b82528805ceef05a3d40
OikeaHetki <zekthesnek@gmail.com>
2025-06-03 06:31:12 -0400

    fabled extra deck monsters

--- script/c2600000123.lua
+++ script/c2600000123.lua
@@ -39,4 +39,14 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
+	if c:IsRelateToEffect(e) then
+		--Cannot attack
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3206)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
 end
