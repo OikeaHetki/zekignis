3582d26dbf13cf43d8b777b322dc640ccabf3b1a
OikeaHetki <zekthesnek@gmail.com>
2025-08-07 17:11:47 -0400

    VALGAVOTH

--- script/c2600001555.lua
+++ script/c2600001555.lua
@@ -1,9 +1,8 @@
 --恐怖を喰うもの、ヴァルガヴォス
 --Valgavoth, Terror Eater
 --zek
-local s,id,alias=GetID()
+local s,id=GetID()
 function s.initial_effect(c)
-	alias=c:GetOriginalCodeRule()
 	--xyz summon
 	Xyz.AddProcedure(c,nil,9,3,nil,nil,nil,nil,false,s.xyzcheck)
 	c:EnableReviveLimit()
@@ -25,7 +24,6 @@ function s.initial_effect(c)
 	e2:SetTargetRange(0,0xff)
 	e2:SetValue(LOCATION_REMOVED)
 	e2:SetTarget(s.rmtg)
-	e2:SetCondition(s.mcon)
 	c:RegisterEffect(e2)
 	--recover
 	local e3=Effect.CreateEffect(c)
@@ -43,8 +41,7 @@ function s.xyzfilter(c,xyz,tp)
 	return c:IsAttribute(ATTRIBUTE_DARK,xyz,SUMMON_TYPE_XYZ,tp)
 end
 function s.xyzcheck(g,tp,xyz)
-	local mg=g:Filter(function(c) return not c:IsHasEffect(EFFECT_EQUIP_SPELL_XYZ_MAT) end,nil)
-	return mg:IsExists(s.xyzfilter,1,nil,xyz,tp)
+	return g:IsExists(s.xyzfilter,1,nil,xyz,tp)
 end
 function s.mcon(e)
 	return e:GetHandler():GetOverlayCount()>0
@@ -54,9 +51,9 @@ function s.rmtg(e,c)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST)
-		and Duel.CheckLPCost(tp,1500) end
+		and Duel.CheckLPCost(tp,900) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
-	Duel.PayLPCost(tp,1500)
+	Duel.PayLPCost(tp,900)
 end
 function s.filter(c)
 	return c:IsFaceup() and c:IsAbleToHand()
@@ -66,12 +63,12 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_REMOVED,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_REMOVED,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(ev)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,0,0,tp,ev)
 end
-function s.pilop(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Recover(p,d,REASON_EFFECT)
 	local tc=Duel.GetFirstTarget()
