bee0791c658d34e49006ecf32420abe727c0f722
OikeaHetki <zekthesnek@gmail.com>
2025-08-13 09:09:00 -0400

    correction for vanilla monsters

---
+++ script/c2600001561.lua
@@ -0,0 +1,59 @@
+--カオス・フェムトロン
+--Chaos Femtron
+--zek
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,4,2,nil,nil,nil,nil,false,s.xyzcheck,Xyz.InfiniteMats)
+	c:EnableReviveLimit()
+	--Banish 1 monster on the field
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.rmcost)
+	e1:SetTarget(s.rmtg)
+	e1:SetOperation(s.rmop)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+end
+function s.xyzfilter1(c,xyz,tp)
+	return c:IsAttribute(ATTRIBUTE_LIGHT,xyz,SUMMON_TYPE_XYZ,tp)
+end
+function s.xyzfilter2(c,xyz,tp)
+	return c:IsAttribute(ATTRIBUTE_DARK,xyz,SUMMON_TYPE_XYZ,tp)
+end
+function s.xyzcheck(g,tp,xyz)
+	return g:IsExists(s.xyzfilter1,1,nil,xyz,tp) and g:IsExists(s.xyzfilter2,1,nil,xyz,tp)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST)
+		and e:GetHandler():GetAttackAnnouncedCount()==0 end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+	--Cannot attack this turn
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(3206)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_ATTACK)
+	e1:SetReset(RESETS_STANDARD_PHASE_END)
+	e:GetHandler():RegisterEffect(e1,true)
+end
+function s.tgfilter(c)
+	return c:IsFaceup() and c:IsAbleToRemove() and c:IsType(TYPE_EFFECT)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.tgfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
+end
